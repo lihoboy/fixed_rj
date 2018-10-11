468b9a333b73d0f57556df30f8da799910e94594
Hsu,Li-Ho <43001838+lihoboy@users.noreply.github.com>
2018-10-11 10:40:16 +0800

    v6 is good

--- fixed_rj.py
+++ fixed_rj.py
@@ -6,7 +6,7 @@ import DrawGan as DG
 def main():
 
     '''read file'''
-    file_path="data_2_10_v2"
+    file_path="data_2_10_v6"
     num_rj, num_jobs, rj_list, Job_list=read_file("data/"+file_path)
 
     '''init'''
